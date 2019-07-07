b5d341fdaca5610950c396642b638afdf64d339c
Matin Sasanpour <matin.sasan@gmail.com>
2019-07-07 17:06:54 +0430

    added logic for redis and postgres

--- server/index.js
+++ server/index.js
@@ -1,4 +1,12 @@
-const { pgUser, pgHost, pgPassword, pgDatabase, pgPort } = require('./keys');
+const {
+  pgUser,
+  pgHost,
+  pgPassword,
+  pgDatabase,
+  pgPort,
+  redisHost,
+  redisPort
+} = require('./keys');
 
 // express
 const express = require('express');
@@ -23,3 +31,47 @@ pgClient.on('error', () => console.log('PG connection lost!'));
 pgClient
   .query('CREATE TABLE IF NOT EXISTS values (number INT)')
   .catch(err => console.log(err));
+
+// redis client
+const redis = require('redis');
+const redisClient = redis.createClient({
+  host: redisHost,
+  port: redisPort,
+  retry_strategy: () => 1000
+});
+const redisPublisher = redisClient.duplicate();
+
+// express route handlers
+app.get('/', (req, res) => {
+  res.send('Hello there :D');
+});
+
+app.get('/values/everything', async (req, res) => {
+  const values = await pgClient.query('SELECT * FROM values');
+
+  res.send(values.rows);
+});
+
+app.get('/values/current', async (req, res) => {
+  redisClient.hgetall('values', (err, values) => {
+    res.send(values);
+  });
+});
+
+app.post('/values', async (req, res) => {
+  const index = req.body.index;
+
+  if (+index > 40) {
+    return res.status(422).send('index too high');
+  }
+
+  redisClient.hset('values', index, 'Nothing yet :/');
+  redisPublisher.publish('insert', index);
+  pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
+
+  res.send({ working: true });
+});
+
+app.listen(5000, () => {
+  console.log('listening...');
+});
